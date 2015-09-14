cba56af3c43d30a5408bb5d2080f3ae0859f3996
liorboim <liorboim@gmail.com>
2015-09-14 06:19:28 -0700

    cards by classes 2

---
+++ sampleoverwolfextensionlibrary/CardsByClasses.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SampleOverwolfExtensionLibrary
+{
+    public class CardsByClasses
+    {
+       
+
+        public class RootObject
+        {
+            public List<string> Druid { get; set; }
+        }
+
+        private static RootObject m_instanse=null;
+
+        public static RootObject Instanse
+        {
+            get
+            {
+                if (m_instanse == null)
+                {
+                    string overwolfDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                    string configFilePath = Path.Combine(overwolfDir, "Overwolf\\Ninja\\CardsClasses.json");
+                    if (File.Exists(configFilePath))
+                    {
+                        string configuration = File.ReadAllText(configFilePath);
+                        m_instanse = JsonConvert.DeserializeObject<RootObject>(configuration);
+                    };
+                }
+                return m_instanse;
+            }
+            set { }
+        }
+    }
+
+    
+}
